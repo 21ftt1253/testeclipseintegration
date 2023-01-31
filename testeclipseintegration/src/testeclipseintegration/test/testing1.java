2350e701deed3834e9fe5dd375bd67a37c5ac400
Rizal <21ftt1253@student.pb.edu.bn>
2023-01-31 15:27:50 +0800

    I Enter new ClassA

--- testeclipseintegration/src/testeclipseintegration/test/testing1.java
+++ testeclipseintegration/src/testeclipseintegration/test/testing1.java
@@ -3,5 +3,7 @@ package testeclipseintegration.test;
 public class testing1 {
 	public static void main(String[] args) {
 		System.out.println("Github");
+		ClassA classA = new ClassA("ABCDEF");
+		System.out.println(classA.getAbc());
 	}
 }
