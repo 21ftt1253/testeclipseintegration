4641fa42c2e9a1f38841349599e95382adf26193
Rizal <21ftt1253@student.pb.edu.bn>
2023-01-31 16:14:30 +0800

    EHE

--- testeclipseintegration/src/testeclipseintegration/test/testing1.java
+++ testeclipseintegration/src/testeclipseintegration/test/testing1.java
@@ -3,7 +3,7 @@ package testeclipseintegration.test;
 public class testing1 {
 	public static void main(String[] args) {
 		System.out.println("Github");
-		ClassA classA = new ClassA("ABCDEF");
+		ClassA classA = new ClassA("ABCDEFZAL2023");
 		System.out.println(classA.getAbc());
 	}
 }
